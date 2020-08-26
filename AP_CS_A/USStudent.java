718c258a36e1fd01ff9f7172f3cefd8a20d43a58
Monica Ortiz <mortiz@greenwichacademy.org>
2020-08-26 11:38:23 -0400

    ported files to correct repository

--- AP_CS_A/USStudent.java
+++
@@ -1,9 +0,0 @@
-public class USStudent extends UpperSchool{
-  public USStudent(){
-    System.out.println("Do I actually have a uniform?");
-  }
-
-  public int blocksInClass(){
-    return 6;
-  }
-}
