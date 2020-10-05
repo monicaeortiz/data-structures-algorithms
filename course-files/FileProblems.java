5be10fa2dd49c07adb3f6f12ae933e51ae8872ad
Monica Ortiz <mortiz@greenwichacademy.org>
2020-10-05 11:23:34 -0400

    Files and PrintStream

--- course-files/FileProblems.java
+++ course-files/FileProblems.java
@@ -4,10 +4,14 @@ import java.util.*;
 public class FileProblems{
   public static void main(String[] args)
     throws FileNotFoundException{
-    countValues("numbers.txt");
-    System.out.printf("%4.1f\n",calcAverage("numbers.txt"));
-    System.out.println(calcAverages("numberArray.txt"));
-    countCoins(new Scanner(new File("monies.txt")));
+    // countValues("numbers.txt");
+    // System.out.printf("%4.1f\n",calcAverage("numbers.txt"));
+    // System.out.println(calcAverages("numberArray.txt"));
+    // countCoins(new Scanner(new File("monies.txt")));
+
+    //reverseLines(new File("hamlet.txt"), "hamletInReverse.txt");
+    // formatBdays("birthdays.txt", "formattedBirthdays.txt");
+    printDuplicates(new Scanner(new File("dupText.txt")), "duplicates.txt");
   }
 
   // count the number of values in the file
@@ -94,5 +98,109 @@ public class FileProblems{
     System.out.printf("Total money: $%4.2f\n",money);
   }
 
+  // Creates a file where each line is the reverse of the
+  public static void reverseLines(File f, String outputFile)
+    throws FileNotFoundException{
+    // set up PrintStream
+    PrintStream p = new PrintStream(outputFile);
 
+    // open file to Scanner
+    Scanner input = new Scanner(f);
+
+    // grab each lines
+    while(input.hasNextLine()){
+      String line = input.nextLine();
+      String newString = "";
+
+      // reverse the text
+      for (int i = 0; i < line.length(); i++){
+        newString = line.substring(i, i + 1) + newString;
+      }
+      p.println(newString);
+    }
+    input.close();
+    p.close();
+  }
+
+
+  // reformat birthdays
+  public static void formatBdays(String pathIn, String pathOut)
+    throws FileNotFoundException{
+
+      // array for months
+      String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    // set up Scanner and PrintStream
+    Scanner input = new Scanner(new File(pathIn));
+    PrintStream p = new PrintStream(pathOut);
+
+    // while there is still information to look at...
+    while(input.hasNextLine()){
+
+      // grab the next line and parse
+      Scanner line = new Scanner(input.nextLine());
+
+      // parse the line
+      String last = line.next();
+      last = last.substring(0,last.length()-1); // delete comma
+
+      String first = line.next();
+
+      // change delimiter and get date information
+      line.useDelimiter("/");
+      String month = line.next();
+      String day = line.next();
+      String year = line.next();
+
+      p.println(first + " " + last + ": " + months[Integer.parseInt(month.substring(1))-1] + " " + day + ", " + year);
+
+      line.close();
+    }
+    input.close();
+    p.close();
+
+  }
+
+  // prints to a file the duplicate String and the number of duplicates from each line of the input Scanner
+  public static void printDuplicates(Scanner input, String pathOut)
+    throws FileNotFoundException{
+
+    // set up PrintStream output
+    PrintStream p = new PrintStream(pathOut);
+
+    // while there are still more lines to process...
+    while(input.hasNextLine()){
+      // put the line into its own Scanner
+      Scanner line = new Scanner(input.nextLine());
+
+      // what is current word?
+      String currentWord = line.next();
+      // set up counter
+      int count = 1;
+
+      // while there are more words in the line...
+      while(line.hasNext()){
+        // grab the next line and compare to current.
+        String newWord = line.next();
+
+        // if it's the same as the current word, update counter
+        if (newWord.equals(currentWord)){
+          count++;
+        }
+        // if it's different from current word, reset current word and print to file if counter >= 2
+        else{
+          if (count >= 2){
+            p.print(currentWord + "*" + count + " ");
+          }
+          currentWord = newWord;
+          count = 1;
+        }
+      }
+      // print newline to file and go onto next line
+      p.println();
+      line.close();
+    }
+    input.close();
+    p.close();
+
+  }
 }
